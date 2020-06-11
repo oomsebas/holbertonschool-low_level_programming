65ae11d0e7a0de1ced37bcb95c4f5fec8ed29bdc
Sebastian_Orozco <omsebas@gmail.com>
2020-06-11 23:48:30 +0000

    sixth task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int num = 48;
+
+while (num <= 57)
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
+}
