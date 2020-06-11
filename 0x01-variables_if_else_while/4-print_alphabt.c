e8d3a66465dce6334157ef5a0bfaca2365f4f1eb
Sebastian_Orozco <omsebas@gmail.com>
2020-06-11 23:34:15 +0000

    fourth task

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**                                                                                                                                            
- * main - Entry point                                                                                                                          
- *                                                                                                                                             
- * Return: Always 0 (Success)                                                                                                                  
- */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int min = 97;
-  
+
 while (min <= 122)
 {
 if (min == 'q' || min == 'e')
 min++;
 putchar(min);
 min++;
-    }
-  
+}
 putchar('\n');
 return (0);
 }
