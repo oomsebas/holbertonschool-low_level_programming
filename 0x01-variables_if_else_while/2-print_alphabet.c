bbd8033756dd21859799819fbabddd7bd653343e
Sebastian_Orozco <omsebas@gmail.com>
2020-06-11 22:08:05 +0000

    third task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		puttchar(letter);
+		letter++;
+	}
+	putchar('\n')
+	return(0)
+}
