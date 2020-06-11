0fbe8f53c0ca7f6026244a1182ca81872df0b6fb
Sebastian_Orozco <omsebas@gmail.com>
2020-06-11 02:56:27 +0000

    fix on the betty format

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
-/*
-*
-* statements
-*
-*
-*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
