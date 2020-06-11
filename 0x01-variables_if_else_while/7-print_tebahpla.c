5be52719ea87c75a5719a0f7fbd4500fab764714
Sebastian_Orozco <omsebas@gmail.com>
2020-06-11 23:55:04 +0000

    seventh task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter = 122;
+
+	while (letter >= 97)
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}
