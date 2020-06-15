f4971b2cf3072749056f123642d98fcc890caaa9
Sebastian_Orozco <omsebas@gmail.com>
2020-06-15 20:35:33 +0000

    first task

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *
+ *main.c
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	char pr[9]= "Holberton";
+	int  i;
+
+	for(i=0;i<=8;i++)
+	{
+	_putchar(pr[i]);
+	}
+	_putchar(10);
+	return (0);
+}
