f4c33bbabc1ad57a5ef29aaf1b2827e302208fbe
Sebastian_Orozco <omsebas@gmail.com>
2020-06-16 19:48:25 +0000

    Project 0x02 functions and nested loops tasks

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *print_last_digit - prints the last digit of a number.
+ *@c: the number to extract the last digit
+ *
+ *Return: the last digit of c
+ */
+int print_last_digit(int c)
+{
+	c=_abs(c);
+	c = c % 10;
+	_putchar(c + '0');
+	return (c);
+}
