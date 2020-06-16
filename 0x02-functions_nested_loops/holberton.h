63da4c85643ef04b0ae0ee873a2b9c6ce0bb0661
Sebastian_Orozco <omsebas@gmail.com>
2020-06-16 16:56:44 +0000

    change on the holberton.h file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,16 +1,32 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+/**
+ *libraries
+ *
+ */
 #include <unistd.h>
 
+/**
+ * Structs, enums and unions definitions
+ * Typedefs
+ */
+
+/**
+ * Function prototypes
+ */
 
 int _putchar(char c);
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+void print_alphabet(void);
+void print_alphabet_X10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int c);
+int print_last_digit(int c);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
 
+#endif /* _HOLBERTON_H*/
