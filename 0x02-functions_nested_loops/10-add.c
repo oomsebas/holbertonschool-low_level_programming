3ab87f5bc047235efd79d4280f57b5d46fde3fe0
Sebastian_Orozco <omsebas@gmail.com>
2020-06-16 23:44:19 +0000

    tenth task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - sum up two number
+ *@a: number one to sum
+ *@b: number two to sum
+ *
+ * Return: the sum of a and b.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
