b8b98323e8743deaaac22dd007135e1ae008f65d
Sebastian_Orozco <omsebas@gmail.com>
2020-06-16 18:31:57 +0000

    Project 0x02 functions and nested loops tasks

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_X10 - prints the alphabet to stdout ten times
+ *
+ */
+void print_alphabet_X10(void)
+{
+	int n = 97;
+	int m = 0;
+
+	while (m <= 9)
+	{
+		while (n <= 122)
+		{
+			_putchar(n);
+			n++;
+		}
+	_putchar(10);
+	m++;
+	n = 97;
+	}
+}
