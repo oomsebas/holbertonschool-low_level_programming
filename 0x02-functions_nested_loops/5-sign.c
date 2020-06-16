71cc8da6b2f9a14a6077041ad9c83ff2820012f0
Sebastian_Orozco <omsebas@gmail.com>
2020-06-16 19:12:01 +0000

    Project 0x02 functions and nested loops tasks

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *print_sign - Prints the sign of a number
+ *@n: the number to destect its sign
+ *
+ *Return: '+' for positive numbers, '0' for zero, '-' for negative numbers
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
