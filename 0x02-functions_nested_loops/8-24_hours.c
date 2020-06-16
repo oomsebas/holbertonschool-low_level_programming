9268e52449303018f1a94be4bcdd41f607100d8d
Sebastian_Orozco <omsebas@gmail.com>
2020-06-16 21:16:56 +0000

    Project 0x02 functions and nested loops tasks

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ *prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ *
+ */
+void jack_bauer(void)
+{
+	int h = 0, m = 0;
+	int h1, h2, m1, m2;
+	while (h <= 23)
+	{
+		h1 = h / 10;
+		h2 = h % 10;
+		while(m <= 59)
+		{
+			m1  = m / 10;
+			m2  = m % 10;
+			_putchar(h1 + '0');
+			_putchar(h2 + '0');
+			_putchar(':');
+			_putchar(m1 + '0');
+			_putchar(m2 + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+		m = 0;
+       	}
+}
