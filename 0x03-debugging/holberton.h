5eebaeb6b5029a21d379d9cc76e934306e1958e4
Sebastian_Orozco <omsebas@gmail.com>
2020-06-17 03:58:37 +0000

    fourth task

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -7,6 +7,7 @@
  * Typedefs
  * Function prototypes
  */
+void print_remaining_days(int month, int day, int year);
 int largest_number(int a, int b, int c);
 void positive_or_negative(int n);
 #endif /* HOLBERTON_H */
