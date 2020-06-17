83c924b40ca92dd293e7939321f1ccab8b071fa4
Sebastian_Orozco <omsebas@gmail.com>
2020-06-17 00:53:46 +0000

    task one

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,14 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+#include <unistd.h>
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+void positive_or_negative(int n);
+
+#endif /* _HOLBERTON_H*/
