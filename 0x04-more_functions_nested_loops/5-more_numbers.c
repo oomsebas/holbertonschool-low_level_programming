a10d8c4630c187289501f76f8562754e1e01282f
Sebastian_Orozco <omsebas@gmail.com>
2020-06-18 19:41:46 +0000

    More nested functions project

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 
 /**
- *more_numbers - rints 10 times the numbers, from 0 to 14, followed by a new line.
+ *more_numbers - rints 10 times the numbers, from 0 to 14, followed by 
+ *a new line.
  *
  *Return: None
  *
@@ -10,9 +11,9 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for(i = 0; i <= 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for(j = 0; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
 			{
