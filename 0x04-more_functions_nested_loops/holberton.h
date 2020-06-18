83e65d7be546126c21052c194212922d66e04ff4
Sebastian_Orozco <omsebas@gmail.com>
2020-06-18 19:00:23 +0000

    More nested functions project

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -8,5 +8,10 @@
  * Function prototypes
  */
 int _isupper(int c);
-
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
 #endif /* HOLBERTON_H */
