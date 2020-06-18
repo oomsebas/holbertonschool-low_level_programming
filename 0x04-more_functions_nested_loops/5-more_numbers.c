f7632be114d5d92a60f113cee97c1bef353faa08
Sebastian_Orozco <omsebas@gmail.com>
2020-06-18 19:43:19 +0000

    More nested functions project

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- *more_numbers - rints 10 times the numbers, from 0 to 14, followed by 
+ *more_numbers - rints 10 times the numbers, from 0 to 14, followed by
  *a new line.
  *
  *Return: None
