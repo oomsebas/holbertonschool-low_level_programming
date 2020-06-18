38771ab48f32400a902c16096a56f2a0279d7cb7
Sebastian_Orozco <omsebas@gmail.com>
2020-06-18 15:55:33 +0000

    more nested loops project

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *
+ *_isupper - checks for uppercase character
+ *@c: the letter to check
+ *
+ *Return: 1 if c is uppercse, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+int _isupper(int c);
+
+#endif /* HOLBERTON_H */
