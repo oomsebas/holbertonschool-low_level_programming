c5204d15a075f92a174e2bf12398756fd09daa89
Sebastian_Orozco <omsebas@gmail.com>
2020-06-18 22:22:30 +0000

    More nested loops project

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *print_square - prints a square, followed by a new line.
+ *@size: the number of rows a columns of the square.
+ *
+ *Return: None
+ */
+void print_square(int size)
+{
+	int j, n;
+	j = size;
+	n = size;
+
+	while(n--)
+	{
+
+		while(j--)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		j=size;
+	}
+
+}
