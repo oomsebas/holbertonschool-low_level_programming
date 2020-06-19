df24c679ed42aee2b7fdf13f03bc97ab6a84d63b
Sebastian_Orozco <omsebas@gmail.com>
2020-06-19 02:05:56 +0000

    ten task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ *print_square - prints a square, followed by a new line.
+ *@size: the number of rows a columns of the triangle.
+ *
+ *Return: None
+ */
+void print_triangle(int size)
+{
+	int col = size;
+	int row = size;
+	int spaces = size;
+	int counter = 1;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	while (row--)
+	{
+		spaces = size - counter;
+		while (spaces--)
+		{
+			_putchar(' ');
+			col--;
+		}
+		while (col--)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		counter += 1;
+		col = size;
+
+	}
+}
