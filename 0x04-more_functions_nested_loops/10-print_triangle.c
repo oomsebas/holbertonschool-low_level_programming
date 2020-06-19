e912c4b3a6d9ad6ded4fafa446209103bf20e1bc
Sebastian_Orozco <omsebas@gmail.com>
2020-06-19 02:20:55 +0000

    ten task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- *print_square - prints a square, followed by a new line.
+ *print_triangle - prints a triangle, followed by a new line.
  *@size: the number of rows a columns of the triangle.
  *
  *Return: None
