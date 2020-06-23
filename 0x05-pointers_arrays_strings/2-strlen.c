f6fe40fc76d4158d6bf53eee0115a77e3baafac5
Sebastian_Orozco <omsebas@gmail.com>
2020-06-23 19:11:00 +0000

    third task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *_strlen - function that returns the length of a string.
+ *@s: string to measute its length.
+ *
+ *Return: int with the length of n.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while(*s)
+	{
+		len += 1;
+		s = s + 1;
+	}
+
+	return (len);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -10,4 +10,5 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif /* _HOLBERTON_H_ */
