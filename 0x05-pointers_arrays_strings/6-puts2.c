d3842630b26596bee4f4550542972f71a826872b
Sebastian_Orozco <omsebas@gmail.com>
2020-06-24 03:14:34 +0000

    sixth task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,11 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
 
-	while (*(str + i) != '\0')
+	while (*(str) != '\0')
 	{
-			_putchar(*(str + i));
-		i+=2;
+		_putchar(*(str));
+		str += 2;
 	}
 	_putchar('\n');
 }
