8a7f9941f69de4d622d0f850724f73db12587546
Sebastian_Orozco <omsebas@gmail.com>
2020-06-24 23:52:55 +0000

    first advances task

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ *_atoi - function that convert a string to an integer.
+ *@s: the string to
+ *
+ *Return: the integer to print
+ */
+int _atoi(char *s)
+{
+	int len, sign = 0, res = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			sign++;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			res = (res * 10) + (*s - '0');
+		}
+		else if (*s == ';')
+		{
+			break;
+		}
+		s++;
+	}
+	if (sign % 2 != 0)
+	{
+		res = res * (-1);
+	}
+	return (res);
+}
