1ff22d412c7927a1d828e75f3cdb9ddba1afd823
Sebastian_Orozco <omsebas@gmail.com>
2020-06-24 01:10:17 +0000

    fifth task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ *rev_string - function that reverses a string.
+ *@s: the string to reverse.
+ *
+ *Return: None.
+ */
+
+void rev_string(char *s)
+{
+	int len;
+	int i ;
+	char srev[10];
+
+	len = _strlen(s);
+	for (i = 0; i <= len; i++)
+	{
+		*(srev + i) = *(s + (len -1) - i);
+        }
+	for (i = 0; i < len ; i++)
+	{
+		s[i] = srev[i];
+	}
+}
+
+/**
+ *_strlen - function that returns the length of a string.
+ *@s: string to measute its length.
+ *
+ *Return: int with the length of n.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		len += 1;
+		s = s + 1;
+	}
+
+	return (len);
+}
