29e3796d9d7d53e43b1e15c2c5bbf86852a3d341
Sebastian_Orozco <omsebas@gmail.com>
2020-06-24 02:01:14 +0000

    fifth task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,11 +14,11 @@ void rev_string(char *s)
 	char c;
 
 	len = _strlen(s);
-	for (i = 0; i < len/2; i++)
+	for (i = 0; i < len / 2; i++)
 	{
 		c = s[i];
-		s[i] = s[(len-1)-i];
-		s[(len-1)-i] = c;
+		s[i] = s[(len - 1) - i];
+		s[(len - 1) - i] = c;
 	}
 }
 
