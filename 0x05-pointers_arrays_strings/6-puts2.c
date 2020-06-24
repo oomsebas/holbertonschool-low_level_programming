e423e3ec2e1a02dc14bdf97dca69c53e737c3f92
Sebastian_Orozco <omsebas@gmail.com>
2020-06-24 02:41:56 +0000

    sixth task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,36 +8,16 @@
  */
 void puts2(char *str)
 {
-	int len;
-	int i;
+	int i = 0;
 
-	len = _strlen(str);
-	for (i = 0; i < len ; i++)
+
+	while (*(str + i))
 	{
-
-		if (i % 2 == 0)
+		if ( i % 2 == 0)
 		{
 			_putchar(*(str + i));
 		}
+		i++;
 	}
 	_putchar('\n');
-}
-
-/**
- *_strlen - function that returns the length of a string.
- *@s: string to measute its length.
- *
- *Return: int with the length of n.
- */
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s)
-	{
-		len += 1;
-		s = s + 1;
-	}
-
-	return (len);
 }
