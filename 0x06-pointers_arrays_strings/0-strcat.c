7904e76bf0caeeae12dd311aa54178b397253581
Sebastian_Orozco <omsebas@gmail.com>
2020-06-25 20:01:43 +0000

    task number 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,13 +16,13 @@ char *_strcat(char *dest, char *src)
 	len = _strlen(dest);
 	cp = dest + len;
 	*(cp - 1) = 32;         /*add the space between strings*/
-	while ( *src != '\0')
+	while (*src != '\0')
 	{
 		*(cp) = *src;
 		src++;
 		cp++;
 	}
-	return dest;
+	return (dest);
 }
 
 /**
