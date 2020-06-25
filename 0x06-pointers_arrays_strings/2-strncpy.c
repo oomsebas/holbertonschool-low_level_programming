05d5799f4e200b4a4a54c5d5c21d00c3dc4b8e06
Sebastian_Orozco <omsebas@gmail.com>
2020-06-25 22:26:18 +0000

    Task number two

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *_strncpy - a function that copies a string
+ *@dest: destiny to copy the string
+ *@src: source string to be copied.
+ *@n: number of bytes to be copy.
+ *
+ *Return: pointer with the direction of the copied string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *cp;
+	cp=dest;
+
+	while (n--)
+	{
+		*(dest) = *src;
+		src++;
+		dest++;
+	}
+	return (cp);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -11,6 +11,6 @@
 char *_strcat(char *dest, char *src);
 int _strlen(char *s);
 char *_strncat(char *dest, char *src, int n);
-
+char *_strncpy(char *dest, char *src, int n);
 
 #endif /* _SAMPLE_HEADER_H_ */
