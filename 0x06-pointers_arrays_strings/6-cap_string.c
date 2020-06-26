8e7002ea087e0a330d9b41f458e0a9b2098d1379
Sebastian_Orozco <omsebas@gmail.com>
2020-06-26 01:46:17 +0000

    sixth task

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+/**
+ *cap_string - a function that copies a string
+ *@s: destiny to copy the string
+ *
+ *Return: int.
+ */
+char *cap_string(char *s)
+{
+	int i, len;
+
+	len = _strlen(s);
+	for (i=0; i<len; i++)
+        {
+		if ( (s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' ||
+		     s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')'||
+		     s[i] == '{' || s[i] == '}' || s[i] == 9 || s[i] == 32
+		      || s[i] == 10) && ((s[i+1] >= 'a') &&  (s[i+1] <= 'z')))
+		{
+			s[i+1]=s[i+1]-32;
+
+		}
+	}
+
+	return (s);
+}
+
+/**
+ *_strlen - function that returns the length of a string.
+ *@s: string to measute its length.
+ *
+ *Return: int with the length of n.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		len += 1;
+		s = s + 1;
+	}
+
+	return (len);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -12,5 +12,6 @@ char *_strcat(char *dest, char *src);
 int _strlen(char *s);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-
+int _strcmp(char *s1, char *s2);
+char *cap_string(char *);
 #endif /* _SAMPLE_HEADER_H_ */
