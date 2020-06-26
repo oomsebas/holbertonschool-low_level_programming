a9c06834b74bfe8a33a3af6ae024455bf5638911
Sebastian_Orozco <omsebas@gmail.com>
2020-06-26 02:13:53 +0000

    sixth task

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -13,5 +13,5 @@ int _strlen(char *s);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-char *cap_string(char *);
+char *cap_string(char *s);
 #endif /* _SAMPLE_HEADER_H_ */
