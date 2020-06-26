d1db10ebe22dd21aa80cd7cc61c6574f568093ac
Sebastian_Orozco <omsebas@gmail.com>
2020-06-26 04:47:22 +0000

    eight task

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * - function that encodes a string into 1337.
+ *@s: the string to encode
+ *
+ *Return: pointer to the encoded array
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char c[] = {"aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"};
+	char num[] = {"nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmMnN"};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while (c[j] != '\0')
+		{
+			if (s[i] == c[j])
+			{
+				s[i] = num[j];
+				break;
+			}
+			j++;
+		}
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -18,4 +18,5 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *leet(char *s);
+char *rot13(char *s);
 #endif /* _SAMPLE_HEADER_H_ */
