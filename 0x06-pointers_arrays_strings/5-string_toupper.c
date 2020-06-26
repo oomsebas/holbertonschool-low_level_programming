446e2dd5ad06c0992f80e9be9df445e94141aa58
Sebastian_Orozco <omsebas@gmail.com>
2020-06-26 03:32:56 +0000

    fifth task

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,12 +10,13 @@
 char *string_toupper(char *s)
 {
 	int a;
-        for (a = 0; s[a] != 0; a++)
-        {
-                if (s[a] > 90 && s[a] != 10)
-                {
+
+	for (a = 0; s[a] != 0; a++)
+	{
+		if (s[a] > 90 && s[a] != 10)
+		{
 			s[a] = s[a] - 32;
-                }
-        }
-        return (s);
+		}
+	}
+	return (s);
 }
