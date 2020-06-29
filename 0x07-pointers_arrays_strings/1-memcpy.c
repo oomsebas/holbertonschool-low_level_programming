222377e61c7270c270cc5ece0111106006f98eb7
Sebastian_Orozco <omsebas@gmail.com>
2020-06-29 16:44:16 +0000

    second task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 
 /**
- *_memset - a function that fills memory with a constant byte.
+ *_memcpy - a function that fills memory with a constant byte.
  *@dest: space in memory to fill
  *@src: space in memory to copy
  *@n: the number of places to write
  *
- *Return: a pointer to the memory area s.
+ *Return: a pointer to the memory area dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
