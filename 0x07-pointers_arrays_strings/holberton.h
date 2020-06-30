31f67aa5a150337422380d7d11620db94b54b548
Sebastian_Orozco <omsebas@gmail.com>
2020-06-30 04:31:54 +0000

    nine task

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -16,4 +16,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 #endif /* _SAMPLE_HEADER_H_ */
