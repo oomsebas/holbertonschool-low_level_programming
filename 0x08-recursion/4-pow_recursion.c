3da4df6835a0a65267e265a3221803efa161591b
Sebastian_Orozco <omsebas@gmail.com>
2020-07-01 23:50:54 +0000

    Recursion project

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - unction that returns the value of x raised to
+ *the power of y
+ *@x: base.
+ *@y: exponent.
+ * Return: int with the result
+ */
+int _pow_recursion(int x, int y)
+{
+	int res, sign;
+
+	sign = (x < 0 && y % 2 != 0) ? -1 : 1;
+
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+	y--;
+	res = _pow_recursion(x, y);
+	res = res * x;
+	return (res * sign);
+}
