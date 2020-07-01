43d7edc2ad30d3b047d4e4ddefc53ae0a8fcca17
Sebastian_Orozco <omsebas@gmail.com>
2020-07-01 20:41:54 +0000

    Recursion project

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+#This directory contains all the project for programming in C (low level lenguage)
---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif /* _SAMPLE_HEADER_H_ */
