e2b5d63dd9b33740d16d258a95e99212ed5d1efa
Sebastian_Orozco <omsebas@gmail.com>
2020-07-01 22:22:11 +0000

    Recursion project

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *_print_rev_recursion - function that prints a string in reverse.
+ *@s: string to reverse
+ *
+ *Return: none.
+ */
+void _print_rev_recursion(char *s)
+{
+	if ( *s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -9,5 +9,5 @@
 
 int _putchar(char c);
 void _puts_recursion(char *s);
-
+void _print_rev_recursion(char *s);
 #endif /* _SAMPLE_HEADER_H_ */
