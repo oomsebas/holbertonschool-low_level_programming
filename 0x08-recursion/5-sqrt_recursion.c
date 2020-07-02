30e7ce32a833898a35760ec061e98c6e1bf328db
Sebastian_Orozco <omsebas@gmail.com>
2020-07-02 04:59:01 +0000

    Recursive project

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,21 +8,18 @@
  */
 int _sqrt_recursion(int n)
 {
-	int res;
 
 	if (n < 0)
 		return (-1);
-	else if (n == 0 || n == 1)
-		return (n);
-	res = binary_search(1, n, n);
-	return (res);
+	else if (n > 0)
+		return (binary_search(n, 1));
+	return (0);
 }
 
 /**
  *binary_search - function that returns the natural square root of a number.
- *@res: the end of the interval
  *@n: the number for the root
- *
+ *@root: init number
  *Return: None
  */
 
@@ -30,7 +27,7 @@ int binary_search(int n, int root)
 {
 	if (root * root == n)
 		return (root);
-	if (root * root > n)
+	else if (root * root > n)
 		return (-1);
 
 	return (binary_search(n, root + 1));
