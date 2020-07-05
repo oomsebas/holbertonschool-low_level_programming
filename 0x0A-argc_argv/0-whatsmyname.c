62f544296fa23dda07023de0e08b89ca8d852428
Sebastian_Orozco <omsebas@gmail.com>
2020-07-05 23:17:28 +0000

    Argc & argv project

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,14 @@
 #include "holberton.h"
 #include <stdio.h>
 
-int main( int argc __attribute__((unused)), char **argv)
+/**
+ *main- program that prints the name of the program
+ *@argc: integer containing the number of arguments passed to the function
+ *@argv: array of pointers pointing to the arguments passed
+ *
+ *Return: 0 for sucess, 1 for error.
+ */
+int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
