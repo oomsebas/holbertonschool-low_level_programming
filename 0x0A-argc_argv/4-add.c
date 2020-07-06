300c42be7c4a94974e7bda60f61f3a81106467b1
Sebastian_Orozco <omsebas@gmail.com>
2020-07-06 02:46:49 +0000

    Argc & Argv project

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-        if (argc <= 1)
+	if (argc <= 1)
 	{
 		printf("%d\n", 0);
 		return (1);
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 		if (_isdigit(argv[i]))
 		{
 			printf("%s\n", "Error");
-			return(1);
+			return (1);
 		}
 		sum = sum + _atoi(argv[i]);
 	}
