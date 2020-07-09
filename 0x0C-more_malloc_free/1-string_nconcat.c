18ad26e01d0551f9f137afcfc3a797a39f6383ef
Sebastian_Orozco <omsebas@gmail.com>
2020-07-09 21:31:58 +0000

    More malloc-free project

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,13 +15,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p;
 
 	if (s1 == NULL)
-		s1 = "\0";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = "\0";
+		s2 = "";
 
 	if (n >= (_strlen(s2) + 1))
 	{
-		n  = _strlen(s2);
+		n = _strlen(s2);
 	}
 
 	size1 = strlen(s1);
