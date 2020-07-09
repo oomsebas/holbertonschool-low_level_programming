4fb8f4def45413e9bc5e43b8aa1919c5daae513f
Sebastian_Orozco <omsebas@gmail.com>
2020-07-09 18:00:57 +0000

    More Malloc-free rpoject

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,20 @@
+#ifndef _SAMPLE_HEADER_H_
+#define _SAMPLE_HEADER_H_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void *malloc_checked(unsigned int b);
+#endif /* _SAMPLE_HEADER_H_ */
