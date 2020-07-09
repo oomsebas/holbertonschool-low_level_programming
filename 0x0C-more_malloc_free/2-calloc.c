85e36a72168c52d6ad13b250078e3008a51e1fd0
Sebastian_Orozco <omsebas@gmail.com>
2020-07-09 23:30:02 +0000

    More malloc-free project

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *_calloc - function that allocates memory for an array, using malloc
+ *@nmemb: direction to copy
+ *@size: direction to the string source
+ *
+ *Return: pointer to the allocated memory.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(nmemb * size + 1);
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+		*(p + i) = 0;
+
+	return (p);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -21,4 +21,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 unsigned int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 #endif /* _SAMPLE_HEADER_H_ */
