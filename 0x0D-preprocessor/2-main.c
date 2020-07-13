65f1fa1bf04d5e21098899533ca57586793e7ce3
Sebastian_Orozco <omsebas@gmail.com>
2020-07-13 22:01:19 +0000

    Preprocessor project

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+
+/**
+ *main - program that prints the source file where it was compiled
+ *Return: None
+ */
 int main(void)
 {
-	printf("%s", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
