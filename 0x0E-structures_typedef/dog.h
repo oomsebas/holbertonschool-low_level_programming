38a8ea4b21dab29af77a3878934ff26da86a72fb
Sebastian_Orozco <omsebas@gmail.com>
2020-07-14 03:30:25 +0000

    Structures-typedef project

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,14 @@
  *Description: this struct contains all the variables related
  *to the dog characteristics
  */
-struct dog_t
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 
 };
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
