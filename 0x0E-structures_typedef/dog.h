690dd0e6f29e01479336795cb74f4c28a1eaf4aa
Sebastian_Orozco <omsebas@gmail.com>
2020-07-14 01:36:51 +0000

    Struct-typedef project

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef __STRUCT__
 #define __STRUCT__
 
+/**
+ *struct dog - structure of a dog
+ *@name: pointer name of the dog
+ *@age: the age of the dog
+ *@owner: pointer to ID to owner of the dog
+ *Description: this struct contains all the variables related
+ *to the dog characteristics
+ */
 struct dog
 {
 	char *name;
 	float age;
-	char* owner;
+	char *owner;
 
 };
 
