3d7a41e6a6ae1f8169a0b8c3c3e8e55761c461f6
Sebastian_Orozco <omsebas@gmail.com>
2020-07-15 20:21:17 +0000

    Function pointers project

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "functions_pointers.h"
+#include <stdio.h>
+
+/**
+ *print_name - function that print the name according to a recieved function
+ *@name: name of the person
+ *@f: function to execute and print the name
+ *Returns: None
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+	f(name);
+
+}
