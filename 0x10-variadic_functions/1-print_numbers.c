1fb8c32bdf00b908132e0bfd6c2ba58c23d14b78
Sebastian_Orozco <omsebas@gmail.com>
2020-07-20 04:07:31 +0000

    variadic functions project

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(ap, int));
 		if (i < (n - 1) && (separator != NULL))
-			printf("%c ", *separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(ap);
