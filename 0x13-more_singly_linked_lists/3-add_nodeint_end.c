5a538fb891ed4eb98069e53b24680c33825a55f1
Sebastian_Orozco <omsebas@gmail.com>
2020-07-28 16:04:17 +0000

    More linked list project

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,12 +14,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 
+	new->n = n;
 	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
-	new->n = n;
+
 	join_tail(*head, new);
 
 	return (new);
