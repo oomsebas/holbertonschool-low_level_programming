544ea8221e4f65030d8b3eb814782d0221b4f117
Sebastian_Orozco <omsebas@gmail.com>
2020-07-28 17:04:29 +0000

    More linked list project

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,10 @@ void free_listint2(listint_t **head)
 
 	listint_t *aux;
 
-	while (*head != NULL)
+	while (*head)
 	{
 		aux = (*head)->next;
 		free((*head));
 		(*head) = aux;
 	}
-	head =  NULL;
 }
