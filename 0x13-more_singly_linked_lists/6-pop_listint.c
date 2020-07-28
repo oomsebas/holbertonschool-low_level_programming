1612d5efb15a7e485a3dd6f102f35e236afaf199
Sebastian_Orozco <omsebas@gmail.com>
2020-07-28 20:12:12 +0000

    More linked list project

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,11 +9,11 @@ int pop_listint(listint_t **head)
 {
 	int n;
 	listint_t *aux;
-        
+
 	if (head == NULL)
 		return (0);
 
-	n =(*head)->n;
+	n = (*head)->n;
 	aux = (*head);
 	(*head) = (*head)->next;
 	free(aux);
