293ec06944d5eb4a622920efa620a5bcda9ea58f
Sebastian_Orozco <omsebas@gmail.com>
2020-07-29 02:43:34 +0000

    More linked lists project

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (!idx && aux == NULL)
 	{
-		return (add_nodeint_end(head, n));
+		return (add_nodeint_end(&aux, n));
 	}
 	if (idx > 0)
 	{
