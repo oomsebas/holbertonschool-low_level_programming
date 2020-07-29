5365b9c0659e104af759b21988dd4c8f7623b938
Sebastian_Orozco <omsebas@gmail.com>
2020-07-29 00:26:31 +0000

    More linked lists project

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,6 +29,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (--idx)
 	{
 		aux = aux->next;
+		if (aux == NULL)
+			retun(NULL);
 	}
 	new->n = n;
 	aux1 = aux->next;
