72e2fdbd40b7b7805149c87ddf81e3086ab122a3
Sebastian_Orozco <omsebas@gmail.com>
2020-07-31 02:49:22 +0000

    Bitwise manipulation project

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *get_bit - get a bit number according to a given index
+ *@n: number to get the index
+ *@index: index to search
+ *Return: the number of the index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i;
+
+	if ( index <= 32)
+	{
+		i = (n >> index) & 1;
+		return (i);
+	}
+	return (-1);
+}
