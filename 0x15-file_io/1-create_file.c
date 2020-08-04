3d414eb38bf42a911a703e3521d922156b6ae328
Sebastian_Orozco <omsebas@gmail.com>
2020-08-04 23:49:59 +0000

    second task correction of flags

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,37 +8,30 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0, len = 0, wr = 0;
-	char *buff;
+	int fd, len = 0, wr = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[i])
+	while (text_content[len])
 	{
 		len++;
-		i++;
 	}
 
-	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	buff = malloc(sizeof(char) * (len + 1));
-	if (buff == NULL)
+	wr = write(fd, text_content, len);
+	if (wr == -1 && text_content != NULL)
 	{
-		return (-1);
-	}
-	wr = write(fd, buff, len);
-	if (wr == 0)
-	{
-		free(buff);
+		close(fd);
 		return (-1);
 	}
 
 	close(fd);
-	free(buff);
 	return (1);
 
 }
