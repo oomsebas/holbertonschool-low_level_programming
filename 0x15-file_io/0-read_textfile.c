db7036420210120760d3128a3f31215c7bf79091
Sebastian_Orozco <omsebas@gmail.com>
2020-08-04 22:16:00 +0000

    First task of file I/O project

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+
+/**
+ *read_textfile - function that reads a text file and prints it to the 
+ *POSIX standard output.
+ *@filename: Pointer to the file to read
+ *@letters: the number of letter of the file
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, numchar;
+	char *buff;
+
+	fd = open(filename, O_RDONLY);
+	if(fd == -1)
+	{
+		return (0);
+	}
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+	{
+		return (0);
+	}
+        numchar = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, numchar);
+
+	return(numchar);
+}
