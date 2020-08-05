408daef33cdd2f3a7dea87f40fb7038aa2dd0f84
Sebastian_Orozco <omsebas@gmail.com>
2020-08-05 00:13:09 +0000

    Second task, add of null content condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,12 +13,6 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[len])
-	{
-		len++;
-	}
-	len++;
-
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 	{
@@ -27,6 +21,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 		return (1);
+
+	while (text_content[len])
+	{
+		len++;
+	}
 
 	wr = write(fd, text_content, len);
 	if (wr == -1)
