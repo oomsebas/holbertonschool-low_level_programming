d1e8db3e00ef82bb2901b8a938fa71a75c70a2a2
Sebastian_Orozco <omsebas@gmail.com>
2020-09-06 19:26:18 +0000

    Task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - print the lenght od a double linked list
+ * @h: the double link list to be measured
+ * return: the number of nodes of the double link list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+
+	size_t i = 0;
+
+	if (h == NULL)
+		return (0);
+	i++;
+	h = h->next;
+	i += dlistint_len(h);
+
+	return (i);
+}
