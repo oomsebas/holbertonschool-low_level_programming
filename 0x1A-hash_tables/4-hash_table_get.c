c10260b3d7a62675af8a6b20f96adca95c481cfd
Sebastian_Orozco <omsebas@gmail.com>
2020-10-02 22:02:20 +0000

    Task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get the value of a key
+ * @ht: the hash table
+ * @key: the key to search for
+ * Return: the value associated with the element, or NULL if fails
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+	unsigned long int key_idx;
+	hash_node_t *tmp;
+
+	key_idx = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[key_idx];
+	for ( ; tmp != NULL; tmp = tmp->next)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			return (tmp->value);
+		}
+	}
+	return (NULL);
+}
