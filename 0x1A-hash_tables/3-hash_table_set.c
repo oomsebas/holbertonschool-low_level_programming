f5e1bfb15910be09172b340e70f3e6cd2180b462
Sebastian_Orozco <omsebas@gmail.com>
2020-10-02 18:29:44 +0000

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -5,7 +5,7 @@
  * @ht: pointer to the hash table
  * @key: the key to calculate the index and to store
  * @value: the value to store
- * return: 1 to sucess, 0 failure
+ * Return: 1 to sucess, 0 failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -17,12 +17,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	key_num = key_index((unsigned char *)key, (unsigned long int)ht->size);
 	entry = ht->array[key_num];
 	tmp = ht->array[key_num];
-	while (entry != NULL)
-	{
-		if (strcmp(tmp->key, key) == 0)
-			return (0);
-		tmp = tmp->next;
-	}
 
 	if (entry == NULL)
 	{
@@ -34,6 +28,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
+		while (entry != NULL)
+		{
+			if (strcmp(tmp->key, key) == 0)
+			{
+				ht->array[key_num]->value = strdup(value);
+				return (1);
+			}
+			tmp = tmp->next;
+		}
 		new = malloc(sizeof(hash_node_t));
 		new->key = strdup(key);
 		new->value = strdup(value);
