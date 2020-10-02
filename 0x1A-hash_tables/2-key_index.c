e0fcaf9f556d01d7460aac32304c1398f561c8cc
Sebastian_Orozco <omsebas@gmail.com>
2020-10-02 16:02:22 +0000

    Task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * key-index - function that returns the index in the hash table
+ * @key: the string to be parsed to the hash function
+ * @size: size of the array of the hash table
+ * Return: the index on the array to store the key and value
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	int hash_num;
