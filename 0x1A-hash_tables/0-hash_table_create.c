96600e030190b0b8ff65aaba0b5ffe2e3d2627ff
Sebastian_Orozco <omsebas@gmail.com>
2020-10-02 15:42:07 +0000

    Fix betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 		return (NULL);
-	new->array = malloc(sizeof(hash_node_t*) * size);
+	new->array = malloc(sizeof(hash_node_t *) * size);
 	new->size = size;
 	if (new->array == NULL)
 		return (NULL);
