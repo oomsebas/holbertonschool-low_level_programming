2d4f23903dff98976cb478b58b07ade0b7da9a86
Sebastian_Orozco <omsebas@gmail.com>
2020-10-02 16:37:00 +0000

    Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - sets the values and key of a list
+ * @ht: pointer to the hash table
+ * @key: the key to calculate the index and to store
+ * @value: the value to store
+ * return: 1 to sucess, 0 failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int key_num;
+	hash_node_t  *new, *entry, *tmp;
+
+	if (key == NULL)
+		return (0);
+	key_num = key_index((unsigned char *)key, (unsigned long int)ht->size);
+	entry = ht->array[key_num];
+	tmp = ht->array[key_num];
+	while (entry != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (0);
+		tmp = tmp->next;
+	}
+
+	if (entry == NULL)
+	{
+		ht->array[key_num] = malloc(sizeof(hash_node_t));
+		ht->array[key_num]->key = strdup(key);
+		ht->array[key_num]->value = strdup(value);
+		ht->array[key_num]->next = NULL;
+		return (1);
+	}
+	else
+	{
+		new = malloc(sizeof(hash_node_t));
+		new->key = strdup(key);
+		new->value = strdup(value);
+		new->next = ht->array[key_num];
+		ht->array[key_num] = new;
+		return (1);
+	}
+
+}
