e9977b703e3df3b3296355d8f2feca306832ba66
Sebastian_Orozco <omsebas@gmail.com>
2020-10-03 01:24:26 +0000

    changes to edge conditions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	key_num = key_index((unsigned char *)key, (unsigned long int)ht->size);
 	tmp = ht->array[key_num];
-	if (ht->array[key_num] != 0)
+	if (ht->array[key_num] != NULL)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
@@ -28,27 +28,40 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		else
 		{
-			new = malloc(sizeof(hash_node_t));
-			if (new == NULL)
-				return (0);
-			new->key = strdup(key);
-			new->value = strdup(value);
-			new->next = ht->array[key_num];
-			ht->array[key_num] = new;
+			add_node(&(ht->array[key_num]), value);
 		}
 
 	}
 	else
 	{
-		ht->array[key_num] = malloc(sizeof(hash_node_t));
-		if (!(ht->array[key_num]))
-			return (0);
-		(ht->array[key_num])->key = strdup(key);
-		(ht->array[key_num])->value = strdup(value);
-		(ht->array[key_num])->next = NULL;
-
+		add_node(&(ht->array[key_num]), value);
 	}
 
 	return(1);
 
+}
+
+/**
+ * add_node- adds a new node at the beginning of a list_t list
+ * @head: The head of the node
+ * @key: key
+ * @value: value
+ * Return: head of a node
+ */
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->key = strdup(key);
+        if (new_node->key == NULL)
+		free(new_node->key);
+	new_node->value = strdup(value);
+        if (new_node->value == NULL)
+		free(new_node->value);
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
 }
