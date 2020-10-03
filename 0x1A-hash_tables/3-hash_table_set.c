b1bfea50dd1341d3890e52a34a1cec4f41a3674a
Sebastian_Orozco <omsebas@gmail.com>
2020-10-03 00:28:20 +0000

    changes to edge conditions

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,14 +10,14 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int key_num;
-	hash_node_t  *new,  *tmp;
+	hash_node_t  *new, *tmp;
 	char *tmp1 = NULL;
 
 	if ((key == NULL) | (*key == '\0') | (ht == NULL) | (value == NULL))
 		return (0);
-	key_num = key_index((unsigned char *)key, (unsigned long int)ht->size);
-	if (key_num >= ht->size)
+	if ((ht->size == 0) | !(ht->array))
 		return (0);
+	key_num = key_index((unsigned char *)key, (unsigned long int)ht->size);
 	tmp = ht->array[key_num];
 	if (tmp == NULL)
 	{
@@ -35,6 +35,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			{
 				tmp1 = tmp->value;
 				tmp->value = strdup(value);
+				if (tmp->value == NULL)
+					return (0);
 				free(tmp1);
 				return (1);
 			}
@@ -42,7 +44,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		new = malloc(sizeof(hash_node_t));
 		new->key = strdup(key);
+		if (new->key == NULL)
+			return (0);
 		new->value = strdup(value);
+		if (new->value == NULL)
+			return (0);
 		new->next = ht->array[key_num];
 		ht->array[key_num] = new;
 		return (1);
