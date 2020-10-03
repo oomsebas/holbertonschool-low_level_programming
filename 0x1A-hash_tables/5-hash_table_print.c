104b9134332fb41de0deb48ed02773e0a35217e0
Sebastian_Orozco <omsebas@gmail.com>
2020-10-03 02:55:27 +0000

    Task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,11 +7,32 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned int i;
+	unsigned int i = 0;
+	unsigned int len;
 
+	if (ht->array == NULL)
+	{
+		printf("{}");
+		return;
+	}
 	for (i = 0; i < ht->size; i++)
 	{
-		if ((ht->array[i] != NULL) & (ht->array[i]  != NULL))
-			printf("{ %s : %s }", ht->array[i]->key, ht->array[i]->value);
+		if (ht->array[i] != NULL)
+		{
+			len = i;
+		}
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL && i != len)
+		{
+			printf("'%s': '%s', ", ht->array[i]->key, ht->array[i]->value);
+		}
+	}
+	if (i)
+	{
+		printf("'%s': '%s'", ht->array[len]->key, ht->array[len]->value);
+		printf("}\n");
 	}
 }
