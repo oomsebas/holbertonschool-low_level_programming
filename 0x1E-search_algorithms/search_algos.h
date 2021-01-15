3a855811f91eace138c3a481f3dbf426a858b100
Sebastian_Orozco <omsebas@gmail.com>
2021-01-15 01:05:00 +0000

    updated header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,4 +9,6 @@ int linear_search(int *array, size_t size, int value);
 void print_array(int low, int high, int *array);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int binary_search2(int *array, int last, size_t size, int value);
 #endif
